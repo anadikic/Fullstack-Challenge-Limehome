0f9eaa32c63ae38e14d2bf1adcfc5964d988e9dd
Ana Dikic <Ana.Dikic.Freelancer@proximity.de>
2023-02-03 13:17:21 +0100

    Added routing and pages

---
+++ frontend/book-suite/views/page-not-found/page-not-found.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { PageNotFoundComponent } from './page-not-found.component'
+
+@NgModule({
+  declarations: [PageNotFoundComponent],
+  providers: [],
+  imports: [CommonModule],
+})
+export class PageNotFoundModule {}
