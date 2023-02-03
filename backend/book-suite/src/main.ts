ef4f7f7bf6a1857c65031d0b9c0d8d7a560557a6
Ana Dikic <Ana.Dikic.Freelancer@proximity.de>
2023-02-03 12:08:39 +0100

    Added swagger

--- backend/book-suite/src/main.ts
+++ backend/book-suite/src/main.ts
@@ -1,6 +1,11 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
@@ -8,6 +13,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  const config = new DocumentBuilder().setVersion('1.0').build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api', app, document);
   const port = 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
